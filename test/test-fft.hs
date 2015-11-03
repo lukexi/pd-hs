2e3873885e00ef38190324fa3cc0654ccc4f8699
Luke Iannini <lukexi@me.com>
2015-11-03 15:18:56 -0800

    Update to nightly including needed packages. Add missing files for mic and fft demos

---
+++ test/test-fft.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE RecordWildCards #-}
+import Game.Pal
+import Graphics.GL
+import Graphics.UI.GLFW.Pal
+import Sound.Pd1
+-- import System.Random
+import Control.Monad
+
+main :: IO ()
+main = do
+  GamePal{..} <- initGamePal "Pd Mic FFT" NoGCPerFrame []
+  addToLibPdSearchPath "test"
+  patch <- makePatch "test/test-fft"
+  
+  let fftArrayName = local patch "mic-fft"
+
+  glClearColor 0.1 0.1 0.1 1
+  whileWindow gpWindow $ do
+    processEvents gpEvents (closeOnEscape gpWindow)
+    
+    mValues <- readArray fftArrayName 0 256
+    forM_ mValues $ \values -> do
+      let r = (values !! 25) / 255 * 10
+      glClearColor r 0 0 1
+    
+    glClear GL_COLOR_BUFFER_BIT
+
+    swapBuffers gpWindow
