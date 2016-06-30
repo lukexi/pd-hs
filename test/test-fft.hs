12bebf7ec8a611bb939adea9014acf1db2a52b7d
Luke Iannini <lukexi@me.com>
2016-06-29 23:29:35 -0700

    Fix fft test

--- test/test-fft.hs
+++ test/test-fft.hs
@@ -1,76 +1,81 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 import Graphics.VR.Pal
 import Graphics.GL
 import Graphics.GL.Pal
-import Graphics.UI.GLFW.Pal
 import Sound.Pd
 import Data.Foldable
-
-data Uniforms = Uniforms 
-  { uMVP :: UniformLocation (M44 GLfloat) } 
+import qualified Data.Vector as V
+import qualified Data.Vector.Storable as VS
+import Control.Monad.Trans
+data Uniforms = Uniforms
+  { uMVP :: UniformLocation (M44 GLfloat) }
   deriving Data
 
-makeLine :: Program -> IO (VertexArrayObject, ArrayBuffer, GLsizei)
+makeLine :: Program -> IO (VertexArrayObject, ArrayBuffer (V3 GLfloat), GLsizei)
 makeLine shader = do
 
-  let verts = flip map [0..255] $ \i -> 
-                let x = (fromIntegral (i::Int) / 255) * 2 - 1
-                in V3 x 0 0
-      vertCount = length verts
-      normals = replicate vertCount (V3 0 0 1)
-  
-  positionsBuffer <- bufferData GL_DYNAMIC_DRAW (concatMap toList verts :: [GLfloat])
-  normalsBuffer   <- bufferData GL_STATIC_DRAW (concatMap toList normals :: [GLfloat])
+    let verts = flip map [0..255] $ \i ->
+                    let x = (fromIntegral (i::Int) / 255) * 2 - 1
+                    in V3 x 0 0
+        vertCount = length verts
+        normals = replicate vertCount (V3 0 0 1)
 
-  vao <- newVAO
-  withVAO vao $ do
-    withArrayBuffer positionsBuffer $ assignFloatAttribute shader "aPosition" GL_FLOAT  3
-    withArrayBuffer normalsBuffer   $ assignFloatAttribute shader "aNormal" GL_FLOAT 3
+    positionsBuffer <- bufferData GL_DYNAMIC_DRAW (verts :: [V3 GLfloat])
+    normalsBuffer   <- bufferData GL_STATIC_DRAW (normals :: [V3 GLfloat])
 
-  return (vao, positionsBuffer, fromIntegral vertCount)
+    vao <- newVAO
+    withVAO vao $ do
+        withArrayBuffer positionsBuffer $ assignFloatAttribute shader "aPosition" GL_FLOAT 3
+        withArrayBuffer normalsBuffer   $ assignFloatAttribute shader "aNormal"   GL_FLOAT 3
 
-fftToVerts :: [GLfloat] -> [V3 GLfloat]
+    return (vao, positionsBuffer, fromIntegral vertCount)
+
+fftToVerts :: V.Vector GLfloat -> V.Vector (V3 GLfloat)
 fftToVerts values = newVerts
   where
-      ys = (/255) <$> values
-      newVerts = flip map (zip [0..] ys) $ \(i, y) ->
-        -- Spread x from -1 to 1
-        let x = (fromIntegral (i::Int) / 256) * 2 - 1
-        in V3 x y 0
+    ys = (/255) <$> values
+    newVerts = flip V.imap ys $ \i y ->
+      -- Spread x from -1 to 1
+      let x = (fromIntegral (i::Int) / 256) * 2 - 1
+      in V3 x y 0
 
 main :: IO ()
 main = withPd $ \pd -> do
-  vrPal@VRPal{..} <- initVRPal "Pd Mic FFT" []
-  addToLibPdSearchPath pd "test"
+    vrPal@VRPal{..} <- initVRPal "Pd Mic FFT" []
+    addToLibPdSearchPath pd "test"
 
-  patch  <- makePatch pd "test/test-fft"
-  
-  let fftArrayName = local patch "mic-fft"
+    patch  <- makePatch pd "test/test-fft"
 
-  shader <- createShaderProgram "test/geo.vert" "test/geo.frag"
-  useProgram shader
-  Uniforms{..} <- acquireUniforms shader
-  (lineVAO, lineBuffer, lineVertCount) <- makeLine shader
+    let fftArrayName = local patch "mic-fft"
 
-  glClearColor 0.01 0.01 0.05 1
-  let player = Pose (V3 0 0 2) (axisAngle (V3 0 1 0) 0)
-  whileVR vrPal $ \headM44 hands events -> do
-    processEvents gpEvents (closeOnEscape gpWindow)
+    shader <- createShaderProgram "test/geo.vert" "test/geo.frag"
+    useProgram shader
+    Uniforms{..} <- acquireUniforms shader
+    (lineVAO, lineBuffer, lineVertCount) <- makeLine shader
 
-    -- Get the latest FFT from Pd
-    mValues <- readArray pd fftArrayName (0::Int) 256
+    glClearColor 0.01 0.01 0.05 1
+    let player = Pose (V3 0 0 2) (axisAngle (V3 0 1 0) 0)
+    whileWindow gpWindow $ do
+        (headM44, events) <- tickVR vrPal (transformationFromPose player)
+        forM_ events (\ev -> case ev of GLFWEvent e -> closeOnEscape gpWindow e; _ -> return ())
 
-    -- Place its values into our line's GL buffer
-    forM_ mValues $ \values -> do
-      let newVerts = fftToVerts values
-      bufferSubData lineBuffer (concatMap toList newVerts)
-    
-    -- Draw the line
-    renderWith vrPal player headM44
-      (glClear (GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT)) 
-      $ \projection eyeView -> do
-          let model = mkTransformation (axisAngle (V3 1 1 0) 0) (V3 0 0 0)
-          uniformM44 uMVP (projection !*! eyeView !*! model)
-          withVAO lineVAO $ 
-            glDrawArrays GL_LINE_STRIP 0 lineVertCount
+        -- Get the latest FFT from Pd
+        mValues <- readArray pd fftArrayName (0::Int) 256
+
+        forM_ mValues $ \values -> do
+            -- Place its values into our line's GL buffer
+            let newVerts = fftToVerts (VS.convert values)
+            newVertsM <- VS.unsafeThaw (V.convert newVerts)
+            bufferSubDataV lineBuffer newVertsM
+
+        -- Draw the line
+        renderWith vrPal headM44
+            $ \projection eyeView -> do
+                (glClear (GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT))
+                let model = mkTransformation (axisAngle (V3 1 1 0) 0) (V3 0 0 0)
+                uniformM44 uMVP (projection !*! eyeView !*! model)
+                withVAO lineVAO $
+                    glDrawArrays GL_LINE_STRIP 0 lineVertCount
+
